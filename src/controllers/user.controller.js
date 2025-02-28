112884c543b43edf0c9899e3a9d62d1cc80a70e6
Shingai Dzinotyiweyi <109967579+C-CREAD@users.noreply.github.com>
2025-02-28 12:11:03 +0200

    Updated user.controller.js
    Changed connection.query to connection.execute

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,41 +4,42 @@ class UserController {
 
     async getUserById(userId) {
         const sql = 'SELECT * FROM users WHERE id = ?';
-        const results = await connection.query(sql, [userId]);
+        const results = await connection.execute(sql, [userId]);
+        console.log("results:",results);
         return results[0];
     }
 
     async getUsersByStatus(status) {
         const sql = 'SELECT * FROM users WHERE status = ?';
-        const results = await connection.query(sql, [status]);
+        const results = await connection.execute(sql, [status]);
         return results;
     }
 
     async getAllUsers() {
         const sql = 'SELECT * FROM users';
-        const results = await connection.query(sql);
+        const results = await connection.execute(sql);
         return results;
     }
 
     async createUser(userData) {
         const sql = 'INSERT INTO users (name, email, password) VALUES (?, ?, ?)';
-        const results = await connection.query(sql, [userData.name, userData.email, userData.password]);
+        const results = await connection.execute(sql, [userData.name, userData.email, userData.password]);
         return results.insertId;
     }
 
     async updateUser(userId, userData) {
         const sql = 'UPDATE users SET name = ?, email = ?, password = ? WHERE id = ?';
-        await connection.query(sql, [userData.name, userData.email, userData.password, userId]);
+        await connection.execute(sql, [userData.name, userData.email, userData.password, userId]);
     }
 
     async updateUserStatus(userId, userData) {
         const sql = 'UPDATE users SET status = ? WHERE id = ?';
-        await connectionquery(sql, [userData.status, userId]);
+        await connection.execute(sql, [userData.status, userId]);
     }
 
     async deleteUser(userId) {
         const sql = 'DELETE FROM users WHERE id = ?';
-        await connection.query(sql, [userId]);
+        await connection.execute(sql, [userId]);
     }
 }
 
