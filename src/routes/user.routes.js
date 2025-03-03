8ab9e95ee8bd2467a6c760dc188addf2c247e2a7
Shingai Dzinotyiweyi <109967579+C-CREAD@users.noreply.github.com>
2025-03-03 16:37:50 +0200

    Update user.routes.js
    Fixing Issues with GET requests

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -6,11 +6,11 @@ const ResponseMessages = require("../utils/ResponseMessages");
 router.use(authenticateToken);
 
 
-router.get("/getUserById", async (req, res, next) => {
-    const user = req.body;
+router.get("/getUserById/:id", async (req, res, next) => {
+    const { id } = req.params;
     console.log("=> getUsersById");
     try{
-        let response = await UserController.getUserById(user);
+        let response = await UserController.getUserById(id);
         ResponseMessages.Success(res, response);
 
     }catch(error){
@@ -19,11 +19,23 @@ router.get("/getUserById", async (req, res, next) => {
 });
 
 
-router.get("/getUsersByStatus", async (req, res, next) => {
-    const user = req.body;
+router.get("/getUsersByStatus/:status", async (req, res, next) => {
+    const { status } = req.params;
     console.log("=> getUsersByStatus");
     try{
-        let response = await UserController.getUsersByStatus(user);
+        let response = await UserController.getUsersByStatus(status);
+        ResponseMessages.Success(res, response[0]);
+
+    }catch(error){
+        ResponseMessages.Error(res, error);
+    }
+});
+
+
+router.get("/getUsersByNullStatus", async (req, res, next) => {
+    console.log("=> getUsersByNullStatus");
+    try{
+        let response = await UserController.getUsersByNullStatus();
         ResponseMessages.Success(res, response[0]);
 
     }catch(error){
