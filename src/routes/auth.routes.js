97d8824c0d6e3b39de44e71d6c55c4e371074a38
Shingai Dzinotyiweyi <109967579+C-CREAD@users.noreply.github.com>
2025-03-05 07:13:55 +0200

    Updated auth.routes.js
    Added the links to refresh the tokens and to log out.

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,8 +2,6 @@ const router = require("express").Router();
 const authController = require("../controllers/auth.controller");
 const ResponseMessages = require("../utils/ResponseMessages");
 
-// let refreshTokens = [];
-
 router.post("/register", async (req, res) => {
   try{
     let body = req.body;
@@ -19,19 +17,44 @@ router.post("/register", async (req, res) => {
 router.post("/login", async(req, res) => {
   try{
     let body = req.body;
-    const response = await authController.login(body);
+    const response = await authController.login(body, res);
+
     ResponseMessages.Success(res, response);
   }catch(error){
     ResponseMessages.Error(res, error);
   }
 });
 
-// Implementing Later
-// router.post("/logout", async(req, res) => {
-//   try{
+router.post('/refresh', async (req, res) => {
+  try{
+    const refreshToken = req.cookies.refreshToken;
 
-//   }
-// })
+    if (!refreshToken) {
+      return ResponseMessages.Error(res, 'Refresh Token Required.');
+    }
+
+    const response = await authController.getRefreshTokens(refreshToken, res);
+    ResponseMessages.Success(res, response);
+  } catch(error){
+    ResponseMessages.Error(res, error);
+  }
+  
+});
+
+router.post("/logout", async(req, res) => {
+  try{
+    const refreshToken = req.cookies.refreshToken;
+
+    if (!refreshToken) return ResponseMessages.Error(res, 'No Refresh Token Found.');
+
+    const response = authController.logout(refreshToken);
+
+    res.clearCookie('refreshToken');
+    ResponseMessages.Success(res, response);
+  } catch(error){
+    ResponseMessages.Error(res, error);
+  }
+});
 
 
 module.exports = router;
